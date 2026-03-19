62260f26b76efdb0683a82943c9cd0238b276b13
jk938 <jk938@github.com>
2026-03-19 18:07:57 +0800

    Fix content display

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
 git add -A
-git commit -m "Update: add article detail page, contact email, sources"
-git push origin main
+git commit -m "Fix content display"
+git push origin gh-pages
 pause
